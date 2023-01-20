cb37168f1813a9878a8e9b533cf2bfeb38a0e36f
Lavanya Natchiappan <lavee.natch@gmail.com>
2023-01-20 14:17:18 -0800

    Create HTML outline, questions in JavaScript, and basic CSS formatting

---
+++ script.js
@@ -0,0 +1,57 @@
+// Create the Questions
+let question1 = {
+    title: "Which of the following are ways to declare a variable?",
+    answers:["let", "var", "const", "all of the above"],
+    correct: 4
+};
+
+let question2 = {
+    title: "How do you start single-line comments in JavaScript?",
+    answers: ["//", "*/", "**", "##"],
+    correct: 1
+};
+
+let question3 = {
+    title: "Which of the following is a valid variable name?",
+    answers: ["my-name", "1stAddress", "$paycheck$", "phone number"],
+    correct: 3
+};
+
+let question4 = {
+    title: "Which of the following is a data type in JavaScript?",
+    answers: ["boolean", "array", "null", "string"],
+    correct: 2
+};
+
+let question5 = {
+    title: "How can you ask the user to type in their favorite pizza topping?",
+    answers: ["alert", "confirm", "prompt", "none of the above"],
+    correct: 3
+};
+
+let question6 = {
+    title: "var fullName = 'John' + 'Smith'; What is the value of fullName?",
+    answers: ["John Smith", "10", "undefined", "JohnSmith"],
+    correct: 4
+};
+
+let question7 = {
+    title: "Which of the following is the 'or' logical operator?",
+    answers: ["||", "**", "&&", "!!"],
+    correct: 1
+};
+
+let question8 = {
+    title: "What surrounds the function body?",
+    answers: ["parantheses", "curly brackets", "brackets", "all of the above"],
+    correct: 2
+};
+
+let question9 = {
+    title: "Which of the following statements about functions are true?",
+    answers: ["Functions are values. They can be assigned, copied, or declared in any place of the code.", 
+    "Function Declarations are processed before the code block is executed.", 
+    "Function Expressions are created when the execution flow reaches them.", 
+    "All of the above"],
+    correct: 4
+};
